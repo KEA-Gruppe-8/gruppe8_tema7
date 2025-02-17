26de8f985ef9ff3b5c6b5c67d0fb9c0359bc20b3
Aljo0004 <aljo0004@stud.kea.dk>
2025-02-17 12:35:49 +0100

    update

---
+++ js/produkt.js
@@ -0,0 +1,49 @@
+const myrecipe = new URLSearchParams(window.location.search).get("id");
+let opskrifter = document.querySelector(".opskrifter");
+
+if (!myrecipe) {
+  produktliste.innerHTML = "<p>Ingen opskrift valgt!</p>";
+} else {
+  fetch(`https://dummyjson.com/recipes/${myrecipe}`)
+    .then((res) => res.json())
+    .then((recipe) => {
+      if (recipe.message) {
+        produktliste.innerHTML = `<p>${recipe.message}</p>`;
+      } else {
+        showRecipe(recipe);
+      }
+    })
+    .catch((error) => console.error("Fejl ved hentning af opskrift:", error));
+}
+
+function showRecipe(recipe) {
+  console.log(recipe);
+  const markup = `
+      <h1 class="produkt_overskrift">${recipe.name}</h1>
+      <div class="produkt_billede_info">
+          <div><img src="${recipe.image}" alt="${recipe.name}"></div>
+          <div>
+              <p>Preparation time: ${recipe.prepTimeMinutes} min</p>
+              <p>Cooking time: ${recipe.cookTimeMinutes} min</p>
+              <p>Servings: ${recipe.servings}</p>
+              <p>Difficulty: ${recipe.difficulty}</p>
+              <p>Cuisine: ${recipe.cuisine}</p>
+              <p>Calories per serving: ${recipe.caloriesPerServing}</p>
+          </div>
+      </div>
+      <div class="opskrift">
+          <div>
+              <p class="title">Ingredients</p>
+              <ul class="ingredients">
+                  ${recipe.ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+              </ul>
+          </div>
+          <div>
+              <h2 class="title">Instructions</p>
+              <ul class="instructions">
+                  ${recipe.instructions.map((instruction) => `<li>${instruction}</li>`).join("")}
+              </ul>
+          </div>
+      </div>`;
+  produktliste.innerHTML = markup;
+}
