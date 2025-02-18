f22e08cb0e56839074139f7a04e82f6670d32732
Aljo0004 <aljo0004@stud.kea.dk>
2025-02-18 08:37:11 +0100

    update

--- js/produkt.js
+++ js/produkt.js
@@ -2,13 +2,13 @@ const myrecipe = new URLSearchParams(window.location.search).get("id");
 let opskrifter = document.querySelector(".opskrifter");
 
 if (!myrecipe) {
-  produktliste.innerHTML = "<p>Ingen opskrift valgt!</p>";
+  opskrifter.innerHTML = "<p>Ingen opskrift valgt!</p>";
 } else {
   fetch(`https://dummyjson.com/recipes/${myrecipe}`)
     .then((res) => res.json())
     .then((recipe) => {
       if (recipe.message) {
-        produktliste.innerHTML = `<p>${recipe.message}</p>`;
+        opskrifter.innerHTML = `<p>${recipe.message}</p>`;
       } else {
         showRecipe(recipe);
       }
@@ -21,8 +21,8 @@ function showRecipe(recipe) {
   const markup = `
       <h1 class="produkt_overskrift">${recipe.name}</h1>
       <div class="produkt_billede_info">
-          <div><img src="${recipe.image}" alt="${recipe.name}"></div>
-          <div>
+          <div class="produkt_img"><img src="${recipe.image}" alt="${recipe.name}"></div>
+          <div  class="produkt_info">
               <p>Preparation time: ${recipe.prepTimeMinutes} min</p>
               <p>Cooking time: ${recipe.cookTimeMinutes} min</p>
               <p>Servings: ${recipe.servings}</p>
@@ -33,17 +33,21 @@ function showRecipe(recipe) {
       </div>
       <div class="opskrift">
           <div>
-              <p class="title">Ingredients</p>
-              <ul class="ingredients">
+                <div class="ingredients">
+                <div class="ingredients_ul">
+                <h2 class="title">Ingredients</h2>
+              <ul>
                   ${recipe.ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
               </ul>
+              </div>
+              </div>
           </div>
           <div>
-              <h2 class="title">Instructions</p>
+              <h2 class="title2">Instructions</h2>
               <ul class="instructions">
                   ${recipe.instructions.map((instruction) => `<li>${instruction}</li>`).join("")}
               </ul>
           </div>
       </div>`;
-  produktliste.innerHTML = markup;
+  opskrifter.innerHTML = markup;
 }
